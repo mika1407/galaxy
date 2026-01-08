c40e96efe6a2685d6dcc5ce6788144d56fd0b5f1
mika1407 <koneautomaatio@hotmail.com>
2026-01-08 20:16:19 +0200

    spaceships added

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ WIDTH, HEIGHT = 800, 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Basic Pygame Window")
 
-BLACK = (0, 0, 0)
+BLACK = (0, 0, 100)
+WHITE = (255, 255, 255)
+
+BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
 
 FPS = 60
+VEL = 5
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
@@ -17,13 +21,37 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMA
 
 def draw_window(red, yellow):
     WINDOW.fill(BLACK)
+    pygame.draw.rect(WINDOW, WHITE, BORDER)
     WINDOW.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WINDOW.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
+def yellow_handle_movement(keys_pressed, yellow):
+        if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:  # LEFT
+            yellow.x -= VEL
+        if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:  # RIGHT
+            yellow.x += VEL
+        if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:  # UP
+            yellow.y -= VEL
+        if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT:  # DOWN
+            yellow.y += VEL
+
+def red_handle_movement(keys_pressed, red):
+        if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:  # LEFT
+            red.x -= VEL
+        if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:  # RIGHT
+            red.x += VEL
+        if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  # UP
+            red.y -= VEL
+        if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT:  # DOWN
+            red.y += VEL
+
+
 def main():
-    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
-    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    # Vaihda SPACESHIP_WIDTH ja SPACESHIP_HEIGHT paikkaa, 
+    # koska alukset on käännetty 90/270 astetta:
+    red = pygame.Rect(700, 300, SPACESHIP_HEIGHT, SPACESHIP_WIDTH)
+    yellow = pygame.Rect(100, 300, SPACESHIP_HEIGHT, SPACESHIP_WIDTH)
 
     clock = pygame.time.Clock()
     running = True
@@ -32,6 +60,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
 
         draw_window(red, yellow)
 
