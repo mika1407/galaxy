7a2b195efe1dccee583b9583c8f5a1c464fa40d2
mika1407 <koneautomaatio@hotmail.com>
2026-01-15 16:21:41 +0200

    pygbag versio

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import asyncio # Lisää tämä alkuuni
 import pygame
 import os
 pygame.font.init()
 pygame.mixer.init()
+
 
 WIDTH, HEIGHT = 800, 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -14,8 +16,8 @@ YELLOW = (255, 255, 0)
 
 BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
 
-BULLET_HIT_SOUND = pygame.mixer.Sound('Assets/Grenade+1.mp3')
-BULLET_FIRE_SOUND = pygame.mixer.Sound('Assets/Gun+Silencer.mp3')
+BULLET_HIT_SOUND = pygame.mixer.Sound('Assets/Grenade+1.ogg')
+BULLET_FIRE_SOUND = pygame.mixer.Sound('Assets/Gun+Silencer.ogg')
 
 HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
 WINNER_FONT = pygame.font.SysFont('comicsans', 100)
@@ -94,76 +96,76 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
         elif bullet.x < 0:
             red_bullets.remove(bullet)
 
-def draw_winner(text):
+async def draw_winner(text):
     draw_text = WINNER_FONT.render(text, 1, WHITE)
     WINDOW.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
                          2, HEIGHT/2 - draw_text.get_height()/2))
     pygame.display.update()
-    pygame.time.delay(5000)
+    await asyncio.sleep(5)
 
-def main():
-    # Vaihdettu SPACESHIP_WIDTH ja SPACESHIP_HEIGHT paikat,koska alukset on käännetty 90/270 astetta
-    red = pygame.Rect(700, 300, SPACESHIP_HEIGHT, SPACESHIP_WIDTH)
-    yellow = pygame.Rect(100, 300, SPACESHIP_HEIGHT, SPACESHIP_WIDTH)
+async def main():
+    while True:
+        red = pygame.Rect(700, 300, SPACESHIP_HEIGHT, SPACESHIP_WIDTH)
+        yellow = pygame.Rect(100, 300, SPACESHIP_HEIGHT, SPACESHIP_WIDTH)
 
-    red_bullets = []
-    yellow_bullets = []
+        red_bullets = []
+        yellow_bullets = []
+        red_health = 10
+        yellow_health = 10
 
-    red_health = 10
-    yellow_health = 10
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
 
-    clock = pygame.time.Clock()
-    running = True
-    while running:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                        bullet = pygame.Rect(
+                            yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
+                        yellow_bullets.append(bullet)
+                        BULLET_FIRE_SOUND.play()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
-                    bullet = pygame.Rect(
-                        yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
-                    yellow_bullets.append(bullet)
-                    BULLET_FIRE_SOUND.play()
+                    if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                        bullet = pygame.Rect(
+                            red.x, red.y + red.height//2 - 2, 10, 5)
+                        red_bullets.append(bullet)
+                        BULLET_FIRE_SOUND.play()
+            
+                if event.type == RED_HIT:
+                    red_health -= 1
+                    BULLET_HIT_SOUND.play()
 
-                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
-                    bullet = pygame.Rect(
-                        red.x, red.y + red.height//2 - 2, 10, 5)
-                    red_bullets.append(bullet)
-                    BULLET_FIRE_SOUND.play()
-        
-            if event.type == RED_HIT:
-                red_health -= 1
-                BULLET_HIT_SOUND.play()
+                if event.type == YELLOW_HIT:
+                    yellow_health -= 1
+                    BULLET_HIT_SOUND.play()
 
-            if event.type == YELLOW_HIT:
-                yellow_health -= 1
-                BULLET_HIT_SOUND.play()
+            winner_text = ""
+            if red_health <= 0:
+                winner_text = "Yellow Wins!"
 
-        winner_text = ""
-        if red_health <= 0:
-            winner_text = "Yellow Wins!"
+            if yellow_health <= 0:
+                winner_text = "Red Wins!"
 
-        if yellow_health <= 0:
-            winner_text = "Red Wins!"
+            if winner_text != "":
+                draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
+                await draw_winner(winner_text)
+                running = False 
+                break
 
-        if winner_text != "":
-            draw_winner(winner_text)
-            break
+            keys_pressed = pygame.key.get_pressed()
+            yellow_handle_movement(keys_pressed, yellow)
+            red_handle_movement(keys_pressed, red)
+            handle_bullets(yellow_bullets, red_bullets, yellow, red)
+            draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
 
-        keys_pressed = pygame.key.get_pressed()
-        yellow_handle_movement(keys_pressed, yellow)
-        red_handle_movement(keys_pressed, red)
+            await asyncio.sleep(0)
 
-        handle_bullets(yellow_bullets, red_bullets, yellow, red)
-
-        draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
-
-    main()
-
+        main()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
